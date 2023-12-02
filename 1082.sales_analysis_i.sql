0a12906ae430ff5877d369472188d11081de3390
Sarvex <sarvex@jatasra.com>
2023-12-02 20:37:28 +0530

    first commit

---
+++ 1082.sales_analysis_i.sql
@@ -0,0 +1,12 @@
+WITH
+    T AS (
+        SELECT
+            seller_id,
+            SUM(price) AS tot,
+            RANK() OVER (ORDER BY SUM(price) DESC) AS rk
+        FROM Sales
+        GROUP BY seller_id
+    )
+SELECT seller_id
+FROM T
+WHERE rk = 1;
---
+++ 1322.ads_performance.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT
+    ad_id,
+    ROUND(IFNULL(SUM(action = 'Clicked') / SUM(action IN ('Clicked', 'Viewed')) * 100, 0), 2) AS ctr
+FROM Ads
+GROUP BY 1
+ORDER BY 2 DESC, 1;
